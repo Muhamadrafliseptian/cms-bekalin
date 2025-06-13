3ae28df08d5e89a3de162dfb6825be75aaf924a6
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2025-06-13 17:53:18 +0700

    push

--- gitpull.sh
+++ gitpull.sh
@@ -1,2 +1,2 @@
-git reset --hard
-git pull https://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@github.com/Muhamadrafliseptian/cms-bekalin.git main
+# git reset --hard
+# git pull https://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@github.com/Muhamadrafliseptian/cms-bekalin.git main
